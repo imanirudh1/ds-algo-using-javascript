0d00bf36ce5bbbdbb72aa25bd1229563017f3904
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-20 20:42:50 +0530

    Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,21 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-// function vowels(str) {
-//   const vowel = ['a', 'e', 'i', 'o', 'u']
-//   let counter = 0
-//   for (let c of str.toLowerCase()) {
-//     if (vowel.includes(c)) {
-//       counter++
-//     }
-//   }
-//   return counter
-// }
+function vowels(str) {
+  const vowel = ['a', 'e', 'i', 'o', 'u']
+  let counter = 0
+  for (let c of str.toLowerCase()) {
+    if (vowel.includes(c)) {
+      counter++
+    }
+  }
+  return counter
+}
 
 // Method 2
-function vowels(str) {
-  const matches = str.match(/[aeiou]/gi)
-  return matches ? matches.length : 0
-}
+// function vowels(str) {
+//   const matches = str.match(/[aeiou]/gi)
+//   return matches ? matches.length : 0
+// }
 
 module.exports = vowels
